f2a051d5efbd6058adef949807be9472a6c63317
Salazar Williams, I <i.salazar.williams@accenture.com>
2020-10-13 11:42:30 -0300

    First UT working

--- src/service/customers.spec.ts
+++ src/service/customers.spec.ts
@@ -1,5 +1,43 @@
 import { getCustomers } from './customers';
+import * as mongodbModule from '../modules/mongodb';
+import { ResumeToken } from 'mongodb';
+
+jest.mock('../modules/mongodb');
+const mocked = mongodbModule as jest.Mocked<typeof mongodbModule>;
+const mongoClient = mocked.mongoClient;
+const findAll = mocked.findAll;
+
+beforeEach(() => {
+    mongoClient.mockClear();
+    findAll.mockClear();
+});
 
 test("Primera prueba Jest", ()=>{
-    expect(true).toBe(true);
+    mongoClient.mockImplementation(() => new Promise((resolve, reject) =>{
+        const client = {
+            db : (dbName:string) =>{
+                return {
+                    collection: (collectionName:string) => {
+                        return collectionName;
+                    }
+                };
+            }
+        }
+        resolve(client);
+    }));
+
+    findAll.mockImplementation(() => new Promise((resolve, reject) =>{
+        const result = {
+            value: "OK"
+        }
+        resolve(result);
+    }));
+
+    const req = {};
+    const res = {
+        send: (response:any) => {
+            expect(response.value).toBe("OK");
+        }
+    }
+    getCustomers(req,res);
 });
